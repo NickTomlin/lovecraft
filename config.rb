8e792af14d10a47ba7e6d8ab18c75d297201e675
Nick Tomlin <nick.tomlin@gmail.com>
2013-05-03 16:11:13 -0500

    compass config

--- config.rb
+++ config.rb
@@ -2,9 +2,9 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
+css_dir = "static"
 sass_dir = "scss"
-images_dir = "img"
+images_dir = "static/img"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
