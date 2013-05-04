8aa8c4d20c38907a854d1b64388e83a1b22f17aa
Nick MBA <nick.tomlin@gmail.com>
2013-05-04 12:41:58 -0500

    created source directory

--- source/config.rb
+++ source/config.rb
@@ -5,7 +5,7 @@ http_path = "/"
 css_dir = "static"
 sass_dir = "scss"
 images_dir = "static/img"
-javascripts_dir = "js"
+javascripts_dir = "static/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
