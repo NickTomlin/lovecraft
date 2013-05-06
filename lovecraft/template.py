5422fbc74735394dfcccdc11f48553eaef528f18
Nick Tomlin <nick.tomlin@gmail.com>
2013-05-06 16:30:25 -0500

    begin a OO based templating system. How to test?

--- lovecraft/template.py
+++ lovecraft/template.py
@@ -1,24 +1,22 @@
-from jinja2 import Environment, FileSystemLoader, PackageLoader
-
-# loader = FileSystemLoader('lovecraft/templates')
-loader = PackageLoader('lovecraft', 'templates')
-env = Environment(loader=loader)
+from jinja2 import Environment as JinjaEnvironment, FileSystemLoader
 
 
-def base_test():
-    return False
-''' @todo explain post object here
-'''
-
-
-def post(post_obj):
-    template = env.get_template('post_single.html')
-    return template.render(post=post_obj)
-
-
-def index(posts_array):
-    ''' What gets weird here is that we are passing an [] of posts,
-    not a post_obj.
+class Environment():
+    ''' A lovecraft wrapper for jinja2. Provide pluggability and customization
+    without hacking **too** much.
     '''
-    template = env.get_template('index.html')
-    return template.render(posts=posts_array)
+
+    def __init__(self, template_dir='source/templates'):
+        self.template_dir = template_dir
+        self.env = JinjaEnvironment(FileSystemLoader(template_dir))
+
+    def base_test(self):
+        return
+
+    def post(self, post_obj):
+        template = self.env.get_template('post_single.html')
+        return template.render(post=post_obj)
+
+    def index(self, posts_array):
+        template = self.env.get_template('index.html')
+        return template.render(posts=posts_array)
