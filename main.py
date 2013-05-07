ac9a4f94a4f1a4f4ab2fb905a12d570acbd990f2
Nick MBA <nick.tomlin@gmail.com>
2013-05-07 13:53:26 -0500

    simplified main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from lovecraft import parse
 
 
 def kick_it():
-    parse.create_posts('content/posts', 'build', 'content/posts')
+    parse.create_posts('content/posts', 'build')
 
 
 if __name__ == '__main__':
