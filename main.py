e702a74f97a9bd9f26c591bd0852c87a6cdfddb5
Nick Tomlin <nick.tomlin@gmail.com>
2013-05-10 14:37:35 -0500

    added simplified arg parsing to main.py

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from lovecraft import parse
+from lovecraft import post
+from os import argv
 
 
-def kick_it():
-    parse.create_posts()
+''' Commands
+1. Clean @todo
+2. Build @todo
+3. Preview @todo
+4. Content types @todo
+5. New Post @doing
+'''
+
+
+def main(args):
+    ''' Parse arguements.
+    I'm doing it wrong. I should use one of the standard [patterns](http://docs.python.org/2/library/getopt.html#getopt.error)
+    For a resource, look at David Warings [RTM cli arg parsing](https://bitbucket.org/dwaring87/rtm-cli/src/2a5f697807b4b9f106081352e26e84b6d1e49751/rtm?at=default#cl-4388)
+    '''
+    if args[0] == 'post':
+        if args[1]:
+            with open(args[1], 'w+') as postfile:
+                post = post.Post(title=args[1])
+                postfile.write(post.content)
+                print ("Created post")
+        else:
+            print ('No post title specified')
+
+    elif args[0] == 'build':
+        parse.create_posts()
+
+    else:
+        print('No arguements supplied. Peace out.')
 
 
 if __name__ == '__main__':
-    kick_it()
+    main(argv[1:])
