50939bbcf5bb1084ac757a386ab7c00a4c25a65c
Nick MBA <nick.tomlin@gmail.com>
2013-06-05 20:45:18 -0500

    corrected typo in doc description for get_safe_pathname

--- lovecraft/util.py
+++ lovecraft/util.py
@@ -3,7 +3,7 @@ import re
 
 def get_safe_pathname(path):
     ''' (str) -> (str)
-    returns a lowercased "safe" version of a string with spaces replaced by "_" and
+    returns a lowercased "safe" version of a string with spaces replaced by "_"
     '''
     path_re = r'(\s+)'
     safe = re.sub(path_re, '_', path)
